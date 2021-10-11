cc3a37596c7aa01be96f2d48843f5e09348dc7db
AdrielRosario <adriel15rosario@gmail.com>
2021-10-10 23:21:25 -0400

    first commit

---
+++ DependencyServiceSample/DependencyServiceSample/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DependencyServiceSample
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer platformInitializer) : base(platformInitializer) 
+        {
+            InitializeComponent();
+
+            
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+
+    }
+}
